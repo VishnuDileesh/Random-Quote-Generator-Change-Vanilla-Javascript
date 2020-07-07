b6b6e3e308021f9dd1791946dc41e629618f6be7
VishnuDileesh <dev@vishnudileesh.com>
2020-07-07 21:39:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random Quote Generator | Vanilla Javascript | Project 03
+
+> jsbeginners.com :
+> https://jsbeginners.com/random-quotes-project
+
+> Checkout Project on Codepen : https://codepen.io/vishnu_dileesh/pen/KKVoojX
+
+![Random Quote Generator | Vanilla Javascript project |](screenshot-codepen-js-project.png)
---
+++ script.js
@@ -0,0 +1,59 @@
+const quotes = [
+  {
+    quote:
+      "When something is important enough, you do it even if the odds are not in your favor.",
+    author: "Elon Musk",
+  },
+
+  {
+    quote:
+      "Design is not just what it looks like and feels like. Design is how it works.",
+    author: "Steve Jobs",
+  },
+
+  {
+    quote:
+      "Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
+    author: "Bill Gates",
+  },
+
+  {
+    quote: "Solving big problems is easier than solving little problems.",
+    author: "Sergey Brin",
+  },
+
+  {
+    quote: "Price is what you pay. Value is what you get.",
+    author: "Warrent Buffett",
+  },
+
+  {
+    quote:
+      "Without internet, there would have been no Jack Ma, and no Alibaba or Taobao.",
+    author: "Jack Ma",
+  },
+
+  {
+    quote:
+      "I think a simple rule of business is, if you do the things that are easier first, then you can actually make a lot of progress.",
+    author: "Mark Zukerberg",
+  },
+
+  {
+    quote:
+      "You don't learn to walk by following rules. You learn by doing, and by falling over.",
+    author: "Richard Branson",
+  },
+
+  {
+    quote:
+      "If you aren't embarrassed by the first version of your product, you shipped too late.",
+    author: "Ried Hoffman",
+  },
+
+  {
+    quote:
+      "Stone Age. Bronze Age. Iron Age. We define entire epics of humanity by the technology they use.",
+    author: "Reed Hastings",
+  },
+];
